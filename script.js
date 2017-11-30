de7c20151c8d3d2d299599d574a6b7829ef68818
Thomas Sanlis <Thomas.sanlis@gmail.com>
2017-11-30 20:02:15 +0100

    :art: Better code

--- script.js
+++ script.js
@@ -6,6 +6,22 @@ const taskColumn1 = document.querySelector('#c1'),
   taskColumn3 = document.querySelector('#c3'),
   taskColumn4 = document.querySelector('#c4');
 
+/* * * *
+ * DEFAULT STYLES
+ * * * */
+const dc1 = '.habit{display:block!important;}';
+const dc2 = '.daily{display:block!important;}';
+const dc3 = '.todo{display:block!important;}';
+const dc4 = '.reward{display:block!important;}';
+
+/* * * *
+ * CUSTOM STYLES
+ * * * */
+const cc1 = '.habit{display:none!important;}';
+const cc2 = '.daily{display:none!important;}';
+const cc3 = '.todo{display:none!important;}';
+const cc4 = '.reward{display:none!important;}';
+
 let style = {
   id: 12,
   'c1': '',
@@ -23,6 +39,8 @@ const saveStyle = function () {
     'style': JSON.stringify(style)
   }, function () {});
 
+  console.log(style);
+
   chrome.tabs.executeScript(null, {
     file: "inject.js"
   });
@@ -36,16 +54,16 @@ const loadStyle = function () {
 
   chrome.storage.sync.get('style', function (item) {
     var save = JSON.parse(item['style']);
-    if (save.c1.length > 1) {
+    if (save.c1 === cc1) {
       taskColumn1.checked = false;
     }
-    if (save.c2.length > 1) {
+    if (save.c2 === cc2) {
       taskColumn2.checked = false;
     }
-    if (save.c3.length > 1) {
+    if (save.c3 === cc3) {
       taskColumn3.checked = false;
     }
-    if (save.c4.length > 1) {
+    if (save.c4 === cc4) {
       taskColumn4.checked = false;
     }
   });
@@ -69,36 +87,36 @@ style.default = `
  * * * */
 taskColumn1.addEventListener('click', function () {
   if (!this.checked) {
-    style.c1 = '.habit{display:none!important;}';
+    style.c1 = cc1;
   } else {
-    style.c1 = '.habit{display:block!important;}';
+    style.c1 = dc1;
   }
   saveStyle();
 });
 
 taskColumn2.addEventListener('click', function () {
   if (!this.checked) {
-    style.c2 = '.daily{display:none!important;}';
+    style.c2 = cc2;
   } else {
-    style.c2 = '.daily{display:block!important;}';
+    style.c2 = dc2;
   }
   saveStyle();
 });
 
 taskColumn3.addEventListener('click', function () {
   if (!this.checked) {
-    style.c3 = '.todo{display:none!important;}';
+    style.c3 = cc3;
   } else {
-    style.c3 = '.todo{display:block!important;}';
+    style.c3 = dc3;
   }
   saveStyle();
 });
 
 taskColumn4.addEventListener('click', function () {
   if (!this.checked) {
-    style.c4 = '.reward{display:none!important;}';
+    style.c4 = cc4;
   } else {
-    style.c4 = '.reward{display:block!important;}';
+    style.c4 = dc4;
   }
   saveStyle();
 });
